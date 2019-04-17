911185951ea38d50a21285f82fd9a28c2e0833fa
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-17 21:14:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,6 @@ class App < Sinatra::Base
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-    binding.pry
   end
   
   get '/operataion/:number1/:number2' do 
