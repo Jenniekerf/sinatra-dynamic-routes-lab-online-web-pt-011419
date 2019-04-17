5836c5856b85fe887e404d7a99b0439a3bd35207
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-17 21:10:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,6 @@ class App < Sinatra::Base
     num2 = params[:number2].to_i
     answer = ""
     case params[:operation]
-    #binding.pry
     when 'add'
       answer = (num1 + num2).to_s 
       when 'subtract'
